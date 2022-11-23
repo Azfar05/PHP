e0a0e816c3426407aac3d1d56b2ebb51da3ab54f
Azfar05 <112875863+Azfar05@users.noreply.github.com>
2022-11-23 09:34:28 +0100

    Add files via upload

--- PHP/week1/opdracht 2/index.php
+++ PHP/week1/opdracht 2/index.php
@@ -5,9 +5,16 @@ echo "<h1>Hallo wereld,</h1>";
 echo "Ik zit op het ";
 
 // Voorbeeld van een variabele in PHP.
-$schoolnaam = "Techniek College Rotterdam !";
+$schoolnaam = "Techniek College Rotterdam !<br>";
 
 // Met echo kun je iets op het scherm tonen.
 echo "$schoolnaam";
+
+echo "Ik maak nu ";
+
+// Mijn eigen toegevoegde variabele.
+$opdracht = "opdracht 2";
+
+echo "$opdracht";
 
 ?>
