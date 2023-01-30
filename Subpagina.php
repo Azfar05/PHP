aa352128928a8da86ec8076a34959eb7258899f6
Azfar05 <112875863+Azfar05@users.noreply.github.com>
2023-01-30 15:24:37 +0100

    Add files via upload

---
+++ Subpagina.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Subpagina 30-1-2023</title>
+</head>
+<body>
+    
+    <nav style="background-color: crimson; width: 100vw; height: 10vh;">
+        <h1>Subpagina</h1>
+        
+        <ul>
+            <li><a href="/index.html">Klik hier</a></li>
+        </ul>
+    </nav>
+
+    <?php
+echo date("l") . "<br>";
+echo date("l jS \of F Y h:i:s A");
+?>
+</body>
+</html>
