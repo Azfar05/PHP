59580d850eb889207d85661aeb584c5c6172a0f2
Azfar05 <112875863+Azfar05@users.noreply.github.com>
2023-06-15 11:14:06 +0200

    Add files via upload

---
+++ LOGIN/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <h1>PDO Login and Registration</h1>
+        <br>
+    </nav>
+
+    <section>
+        <h1>Welkom om de HOME-Pagina!</h1>
+        <br>
+        <p>U bent niet ingelogd. Log in om verder te gaan.</p>
+        <a href="login_form.php">Login</a>
+    </section>
+
+</body>
+</html>
---
+++ LOGIN/login_form.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <h1>PHP-PDO Login and Registration</h1>
+        <br>
+    </nav>
+
+    <section>
+        <h1>login here...</h1>
+        <br>
+    </section>
+
+    <section>
+        <form>
+            <label for="uname">Username:</label>
+            <input type="text" id="uname" name="uname"><br>
+            <label for="password">Password:</label>
+            <input type="text" id="password" name="password">
+          </form>
+    </section>
+</body>
+</html>
